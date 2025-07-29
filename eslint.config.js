761cbbe6f248c01fade692ca164c0252356ceefb
jayschaaf71 <jayschaaf71@gmail.com>
2025-07-29 10:44:55 -0500

    fix: Resolve ESLint errors and improve test script

--- eslint.config.js
+++ eslint.config.js
@@ -25,6 +25,7 @@ export default tseslint.config(
       ],
       "@typescript-eslint/no-unused-vars": "off",
       "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-unused-expressions": ["error", { allowShortCircuit: true, allowTernary: true }],
       "react-hooks/exhaustive-deps": "warn",
     },
   }
